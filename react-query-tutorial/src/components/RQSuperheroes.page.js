3d596647dbc28d34020f40ecb5a23c26c8459834
Bhupendra <bhupendranagda99@gmail.com>
2022-06-18 10:42:53 +0530

    select and custom hook for use query

--- react-query-tutorial/src/components/RQSuperheroes.page.js
+++ react-query-tutorial/src/components/RQSuperheroes.page.js
@@ -1,27 +1,21 @@
-import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetcherFunction = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
-
-const onSuccess = (data) => {
-  console.log("hi i am from on sucess", data);
-};
-
-const onError = (err) => {
-  console.log("hi iam from on error ", err);
-};
+import { useState } from "react";
+import { useSuperHeroesData } from "../hook/useSuperHeroesData";
 
 const RQSuperheroes = () => {
-  const { data, isLoading, isError, error, refetch, isFetching } = useQuery(
-    "super-heroes",
-    fetcherFunction,
-    {
-      onSuccess,
-      onError,
-    }
-  );
+  const [reFetchInterval, setreFetchInterval] = useState(2000);
+
+  const onSuccess = (data) => {
+    setreFetchInterval(false);
+    console.log("hi i am from on sucess", data);
+  };
+
+  const onError = (err) => {
+    setreFetchInterval(false);
+    console.log("hi iam from on error ", err);
+  };
+
+  const { data, isLoading, isError, error, refetch, isFetching } =
+    useSuperHeroesData(onSuccess, onError);
 
   if (isLoading || isFetching) {
     console.log("isLoading");
@@ -35,8 +29,12 @@ const RQSuperheroes = () => {
   return (
     <>
       <h2>RQSuperheroes</h2>
-      {data?.data.map((hero) => {
+      <button onClick={refetch}>Refetch Data</button>
+      {/* {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
+      })} */}
+      {data.map((hero) => {
+        return <div key={hero}>{hero}</div>;
       })}
     </>
   );
---
+++ react-query-tutorial/src/hook/useSuperHeroesData.js
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetcherFunction = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+  return useQuery("super-heroes", fetcherFunction, {
+    onSuccess,
+    onError,
+    select: (data) => {
+      const superHeroes = data.data.map((d) => d.name);
+      return superHeroes;
+    },
+  });
+};
